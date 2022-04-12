a0526bb812bd30bda673becf05e5aa19056f99d7
Josse Angel Niño <jnino@CLRTLC02G54T8MD6N.local>
2022-04-12 17:40:22 -0400

    first commit

---
+++ src/main/java/com/escalab/mediappbackend/MediappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.escalab.mediappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediappbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escalab/mediappbackend/MediappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escalab.mediappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediappbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
